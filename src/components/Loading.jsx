dd9202e0a30745f5066b46c7e6e6f19d82dbab7c
Guga Zimmermann <gugazimmermann@gmail.com>
2022-07-30 22:09:57 -0300

    change loading colors

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,11 +1,11 @@
 export default function Loading() {
 	return (
-		<div className="fixed inset-0 bg-grey-800 bg-opacity-50 overflow-y-auto h-full w-full">
-			<div className="flex flex-col justify-center items-center h-full w-full z-50">
-				<div className="mb-4 text-white text-2xl font-bold" data-testid="loading">
+		<div className="fixed inset-0 bg-gray-900 bg-opacity-90 overflow-y-auto h-full w-full z-50">
+			<div className="flex flex-col justify-center items-center h-full w-full">
+				<div className="mb-4 text-primary text-2xl font-bold" data-testid="loading">
 					Carregando...
 				</div>
-				<div className="animate-spin rounded-full h-32 w-32 border-b-8 border-white" />
+				<div className="animate-spin rounded-full h-32 w-32 border-b-8 border-primary" />
 			</div>
 		</div>
 	);

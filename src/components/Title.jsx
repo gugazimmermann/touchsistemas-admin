3aa116f175f02ed7ac7fce4c590fa21d310c5270
Guga Zimmermann <gugazimmermann@gmail.com>
2022-09-04 22:14:42 -0300

    refactory survey

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,12 +1,17 @@
 import { Link } from 'react-router-dom';
 
-export default function Title({ text, color, back }) {
+export default function Title({ text, color, back, onClick }) {
 	return (
 		<h2 className={`${!color ? 'text-primary' : color} font-bold text-xl mb-2`}>
 			{back && (
 				<Link to={back}>
 					<i className="bx bx-left-arrow-circle mr-2" />
 				</Link>
+			)}
+			{onClick && (
+				<button type="button" onClick={onClick}>
+					<i className="bx bx-minus-circle mr-2" />
+				</button>
 			)}
 			{text}
 		</h2>
